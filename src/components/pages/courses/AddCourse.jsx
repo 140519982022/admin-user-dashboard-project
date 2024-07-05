36f85e8cbc67870d6545aac62d19c8e29682ef48
nikita <you@excnikitakose1414@gmail.com>
2024-07-05 19:33:46 +0530

    view api created by me fetch successfullygit status!

--- src/components/pages/courses/AddCourse.jsx
+++ src/components/pages/courses/AddCourse.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../common/Header'
 import Sidebar from '../../common/Sidebar'
 import Footer from '../../common/Footer'
+import axios, { toFormData } from 'axios'
 
 export default function AddCourse() {
+
+    const [formData, setFormData] = useState({
+
+        course_name: '',
+        price: '',
+        duration: '',
+        description: '',
+        images: '',
+        status: ''
+
+    });
+
+    let formDataStore = (event) => {
+
+    //    let course_name: event.target.course_name;
+    //    let price: event.target.price;
+    //    let duration: event.target.duration;
+    //    let description: event.target.description;
+    //    let images: event.target.images;
+    //    let status: event.target.status;
+
+        let inputName = event.target.name;
+        let inputValue = event.target.value;
+
+        console.log(inputName);
+        console.log(inputValue);
+
+        let object = { ...formData };
+
+        object[inputName] = inputValue;
+
+        setFormData(object);
+
+    }
+
+
+    let saveFomeData = (event) => {
+        // alert("hihih");
+
+
+        event.preventDefault();
+        console.log(formData);
+
+        axios.post(`http://localhost:4000/api/backend/categories/add`, toFormData(formData))
+        .then(response => {
+            console.log(response.data);
+          })
+          .catch(error => {
+            console.log('There was an error!', error);
+          });
+
+
+
+    }
+
+
+
     return (
         <>
             <div className='container-fluid'>
@@ -12,54 +70,54 @@ export default function AddCourse() {
                     <div className='main-content'>
                         <Header />
                         <div className='p-5'>
-                        <h1 className='fw-bold text-danger'>Add course</h1>
+                            <h1 className='fw-bold text-danger'>Add course</h1>
 
-<div className='container'>
-    {/* <div className=''> */}
-    {/* <div className='container'> */}
-    <div className='w-100 mx-auto  m-3 shadow-lg p-5 bg-body rounded border'>
-        <form>
-            {/* <h1 className='fw-bold pb-5 text-danger'>Add course</h1> */}
-            <div className="form-group">
-                <label for="exampleInputEmail1" className='fw-bold py-3'>Course Name</label>
-                <input type="text" className="form-control mb-3" aria-describedby="emailHelp" placeholder="Enter course name" />
-            </div>
-            <div className="form-group">
-                <label for="exampleInputPassword1" className='fw-bold py-3'>Course Price</label>
-                <input type="text" className="form-control" placeholder="enter course price" />
-            </div>
-            <div className="form-group">
-                <label for="exampleInputPassword1" className='fw-bold py-3'>Course Duration</label>
-                <input type="text" className="form-control" placeholder="enter course duration" />
-            </div>
-            <div className="form-group">
-                <label for="exampleInputPassword1" className='fw-bold py-3'>Course Description</label>
-                <textarea name="" className="form-control" rows={3} placeholder='enter course desciption'></textarea>
-            </div>
-            <div className="form-group">
-                <label for="exampleInputEmail1" className='fw-bold py-3'>Course Image</label>
-                <input type="file" className="form-control mb-3" aria-describedby="emailHelp" placeholder="select logo" />
-            </div>
-            <div className="form-group pb-5">
-                <label for="exampleInputEmail1" className='fw-bold py-3'>Course Status</label> <br />
-                <input type="radio" name="course_status" /> &nbsp;&nbsp;&nbsp;
-                <label for="active" className='fw-bold py-2'>Active</label>&nbsp;&nbsp;&nbsp;
+                            <div className='container'>
+                                {/* <div className=''> */}
+                                {/* <div className='container'> */}
+                                <div className='w-100 mx-auto  m-3 shadow-lg p-5 bg-body rounded border'>
+                                    <form onSubmit={saveFomeData}>
+                                        {/* <h1 className='fw-bold pb-5 text-danger'>Add course</h1> */}
+                                        <div className="form-group">
+                                            <label for="exampleInputEmail1" className='fw-bold py-3'>Course Name</label>
+                                            <input type="text" name='course_name' className="form-control mb-3" aria-describedby="emailHelp" placeholder="Enter course name" value={formData.course_name} onChange={formDataStore} />
+                                        </div>
+                                        <div className="form-group">
+                                            <label for="exampleInputPassword1" className='fw-bold py-3'>Course Price</label>
+                                            <input type="text" name='price' className="form-control" placeholder="enter course price" value={formData.price} onChange={formDataStore}/>
+                                        </div>
+                                        <div className="form-group">
+                                            <label for="exampleInputPassword1" className='fw-bold py-3'>Course Duration</label>
+                                            <input type="text" name='duration' className="form-control" placeholder="enter course duration" value={formData.duration} onChange={formDataStore}/>
+                                        </div>
+                                        <div className="form-group">
+                                            <label for="exampleInputPassword1" className='fw-bold py-3'>Course Description</label>
+                                            <textarea name="description" className="form-control" rows={3} placeholder='enter course desciption' value={formData.description} onChange={formDataStore}></textarea>
+                                        </div>
+                                        <div className="form-group">
+                                            <label for="exampleInputEmail1" className='fw-bold py-3'>Course Image</label>
+                                            <input type="file" name='images' className="form-control mb-3" aria-describedby="emailHelp" placeholder="select logo" value={formData.images} onChange={formDataStore} />
+                                        </div>
+                                        <div className="form-group pb-5">
+                                            <label for="exampleInputEmail1" className='fw-bold py-3'>Course Status</label> <br />
+                                            <input type="radio" name="status" /> &nbsp;&nbsp;&nbsp;
+                                            <label for="active" className='fw-bold py-2' value={formData.status} onChange={formDataStore}>Active</label>&nbsp;&nbsp;&nbsp;
 
-                <input type="radio" name="course_status" />&nbsp;&nbsp;&nbsp;
-                <label for="deactive" className='fw-bold py-2'>Deactive</label>
+                                            <input type="radio" name="status" />&nbsp;&nbsp;&nbsp;
+                                            <label for="deactive" className='fw-bold py-2' value={formData.status} onChange={formDataStore}>Deactive</label>
 
-            </div>
-            <button type="submit" className="btn btn-primary fw-bold ">Submit</button>
-            <button type="submit" className="btn btn-warning fw-bold ms-5">Cancel</button>
-        </form>
+                                        </div>
+                                        <button type="submit" className="btn btn-primary fw-bold ">Submit</button>
+                                        <button type="button" className="btn btn-warning fw-bold ms-5">Cancel</button>
+                                    </form>
 
-    </div>
+                                </div>
 
-    {/* </div>
+                                {/* </div>
 
     </div> */}
-    <Footer />
-</div>
+                                <Footer />
+                            </div>
 
                         </div>
                     </div>
--- src/components/pages/courses/VIewCourse.jsx
+++ src/components/pages/courses/VIewCourse.jsx
@@ -1,78 +1,101 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../../common/Sidebar'
 import Header from '../../common/Header'
 import Footer from '../../common/Footer'
 import { CiSearch } from "react-icons/ci";
+import axios from 'axios';
 
 
 export default function VIewCourse() {
-  return (
-    <div className='container-fluid'>
-                <div className='dashboard-wrapper'>
-                    <Sidebar/>
-                    <div className='main-content'>
-                        <Header/>
-                        <div className='p-5'>
-                            <div className='d-flex justify-content-between py-3 w-75'>
-                                <h5 className='text-capitalize text-dark fw-bolder'>
-                                    Welcome To View Courses Table
-                                </h5>
-                                <div class="d-flex" role="search">
-                                    <input className="form-control me-2 position-relative" type="search" placeholder="       Search Mockups Logos.." aria-label="Search" />
-                                    <CiSearch className='position-absolute m-1 fs-3' />
-                                    {/* <button class="btn btn-outline-success" type="submit">Search</button> */}
-                                </div>
+    let [allUser, setAlluser] = useState([])
+
+    let getAllDetails = () => {
+        axios.get(`http://localhost:4000/api/backend/categories/view`)
+            .then((response) => {
+                console.log(response.data[0].data)
+                setAlluser(response.data[0].data)
+            })
+    }
+
+    useEffect(() => {
+        getAllDetails()
+    }, [])
+
+
+    console.log(allUser);
+    return (
+        <div className='container-fluid'>
+            <div className='dashboard-wrapper'>
+                <Sidebar />
+                <div className='main-content'>
+                    <Header />
+                    <div className='p-5'>
+                        <div className='d-flex justify-content-between py-3 w-75'>
+                            <h5 className='text-capitalize text-dark fw-bolder'>
+                                Welcome To View Courses Table
+                            </h5>
+                            <div class="d-flex" role="search">
+                                <input className="form-control me-2 position-relative" type="search" placeholder="       Search Mockups Logos.." aria-label="Search" />
+                                <CiSearch className='position-absolute m-1 fs-3' />
+                                {/* <button class="btn btn-outline-success" type="submit">Search</button> */}
                             </div>
-
-                            <div className='container'>
-                                <div className='container'>
-                                    <div className='w-70 mx-auto p-5 m-5 shadow-lg p-3 bg-body rounded border'>
-
-
-                                        <table class="table mt-5 table-bordered border-dark">
-                                            <thead>
-                                                <tr>
-                                                    <th scope="col">S.no</th>
-                                                    <th scope="col">Slider Heading</th>
-                                                    <th scope="col"><button className='bg-danger text-white me-2'>Delete</button>
-                                                        <input type="checkbox" name="" />
-                                                    </th>
-                                                    <th scope="col">Slider sub-heading</th>
-                                                    <th scope="col">Slider Description</th>
-                                                    <th scope="col">Slider Image</th>
-                                                    <th scope="col">Status</th>
-
-                                                    <th scope="col">Action</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                {/* <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td>@mdo</td>
-                                </tr> */}
-                                                <tr>
-                                                    <th colSpan={8} className='text-center'>Data not found in API</th>
-
-                                                </tr>
-
-                                            </tbody>
-                                        </table>
-
-                                    </div>
-
-                                </div>
-                                <Footer/>
-                            </div>
-
                         </div>
+
+                        <div className='container'>
+                            <div className='container'>
+                                <div className='w-70 mx-auto p-5 m-5 shadow-lg p-3 bg-body rounded border'>
+
+
+                                    <table class="table mt-5 table-bordered border-dark">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">S.no</th>
+                                                <th scope="col">Course</th>
+                                                {/* <th scope="col"><button className='bg-danger text-white me-2'>Delete</button>
+                                                        <input type="checkbox" name="" />
+                                                    </th> */}
+                                                <th scope="col">Description</th>
+                                                <th scope="col">Image</th>
+                                                {/* <th scope="col">Slider Image</th> */}
+                                                <th scope="col">Status</th>
+
+                                                <th scope="col">Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        {allUser.length > 0 ? (
+                                                allUser.map((course, index) => (
+                                                    <tr key={index}>
+                                                        <th scope="row">{index + 1}</th>
+                                                        <td>{course.course_name}</td>
+                                                        <td>{course.description}</td>
+                                                        <td><img src={course.image} alt={course.courseName} style={{ width: '50px' }} /></td>
+                                                        <td>{course.status}</td>
+                                                        <td>
+                                                        <button className='bg-danger text-white me-2 border border-0'>Delete</button>
+                                                        <button className='bg-success text-white me-2 border border-0'>Edit</button>
+
+                                                        </td>
+                                                    </tr>
+                                                ))
+                                            ) : (
+                                                <tr>
+                                                    <th colSpan={6} className='text-center'>Data not found in API</th>
+                                                </tr>
+                                            )}
+
+                                        </tbody>
+                                    </table>
+
+                                </div>
+
+                            </div>
+                            <Footer />
+                        </div>
+
                     </div>
                 </div>
             </div>
-  )
+        </div>
+    )
 }
